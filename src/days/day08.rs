50da97db7f2f5b1247210618b3fae3cabcceec32
Armin <haliovic.armin@hotmail.com>
2023-12-08 16:05:37 +0100

    day 2 part 2 bruteforce

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -50,8 +50,34 @@ fn part_1(mut lines: Lines) -> usize {
     count
 }
 
-fn part_2(lines: Lines) -> usize {
-    0
+fn part_2_bruteforce(mut lines: Lines) -> usize {
+    let instructions: Vec<char> = lines.next().unwrap().chars().collect();
+    let mut instructions_iter = instructions.iter().cycle();
+    let network: HashMap<String, (String, String)> = parse_network(lines);
+    let mut current_nodes: Vec<&String> = network.keys().filter(|x| x.ends_with('A')).collect();
+    let mut count = 0;
+
+    while current_nodes.iter().filter(|x| !x.ends_with('Z')).count() != 0 {
+        count += 1;
+        match instructions_iter.next() {
+            Some(instruction) => {
+                current_nodes = current_nodes
+                    .iter()
+                    .map(|node| match network.get(*node) {
+                        Some((left, right)) => match instruction {
+                            'L' => left,
+                            'R' => right,
+                            _ => panic!("Invalid instruction: {:?}", instruction),
+                        },
+                        None => panic!("Node '{:?}' does not exist in network.", node),
+                    })
+                    .collect();
+            }
+            None => panic!("Ran out of instructions."),
+        }
+    }
+
+    count
 }
 
 pub fn solve() -> SolutionPair {
@@ -59,7 +85,7 @@ pub fn solve() -> SolutionPair {
 
     (
         Solution::from(part_1(contents.lines())),
-        Solution::from(part_2(contents.lines())),
+        Solution::from(part_2_bruteforce(contents.lines())),
     )
 }
 
@@ -77,11 +103,22 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)";
 
-const EXAMPLE_INPUT_2: &str = "LLR
+    const EXAMPLE_INPUT_2: &str = "LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
+
+    const EXAMPLE_INPUT_3: &str = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
 
     #[test]
     fn test_part_1_example() {
@@ -89,8 +126,8 @@ ZZZ = (ZZZ, ZZZ)";
         assert_eq!(part_1(EXAMPLE_INPUT_2.lines()), 6);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT.lines()), 0);
-    // }
+    #[test]
+    fn test_part_2_bruteforce_example() {
+        assert_eq!(part_2_bruteforce(EXAMPLE_INPUT_3.lines()), 6);
+    }
 }

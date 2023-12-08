56d2979cef5db9b15eb97642618089d049e440ac
Armin <haliovic.armin@hotmail.com>
2023-12-08 17:28:07 +0100

    day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -7,8 +7,24 @@ fn load_input(path: &str) -> String {
     fs::read_to_string(path).expect("Could not open file.")
 }
 
-fn part_1(_lines: Lines) -> usize {
-    0
+fn part_1(lines: Lines) -> u32 {
+    lines
+        .map(|line| {
+            let first = line
+                .chars()
+                .skip_while(|c| !c.is_ascii_digit())
+                .next()
+                .unwrap();
+            let last = line
+                .chars()
+                .rev()
+                .skip_while(|c| !c.is_ascii_digit())
+                .next()
+                .unwrap();
+
+            first.to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap()
+        })
+        .sum()
 }
 
 fn part_2(_lines: Lines) -> usize {
@@ -26,16 +42,19 @@ pub fn solve() -> SolutionPair {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT_1: &str = "";
+    const EXAMPLE_INPUT_1: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
     #[test]
     fn test_part_1_example() {
-        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 142);
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(load_input("inputs/day_1").lines()), 0);
+        assert_eq!(part_1(load_input("inputs/day_1").lines()), 55208);
     }
 
     #[test]

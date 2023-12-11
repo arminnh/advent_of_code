826fc68bc07eb7df92838ff1bf753ce927ec60f5
Armin <haliovic.armin@hotmail.com>
2023-12-11 13:11:03 +0100

    day 10 part 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -28,7 +28,7 @@ fn parse_galaxies(lines: Lines) -> HashSet<Position> {
 }
 
 // expand every row and column that does not contain a galaxy
-fn expand_universe(galaxy: HashSet<Position>) -> HashSet<Position> {
+fn expand_universe(galaxy: HashSet<Position>, expansion_factor: usize) -> HashSet<Position> {
     let rows: HashSet<usize> = galaxy.iter().map(|(x, _)| *x).collect();
     let cols: HashSet<usize> = galaxy.iter().map(|(_, y)| *y).collect();
     let max_x: usize = *rows.iter().max().unwrap();
@@ -40,9 +40,11 @@ fn expand_universe(galaxy: HashSet<Position>) -> HashSet<Position> {
     galaxy
         .iter()
         .map(|(x, y)| {
+            let empty_rows_before = empty_x.iter().filter(|empty| *empty < x).count();
+            let empty_cols_before = empty_y.iter().filter(|empty| *empty < y).count();
             (
-                x + empty_x.iter().filter(|empty| *empty < x).count(),
-                y + empty_y.iter().filter(|empty| *empty < y).count(),
+                x + empty_rows_before * (expansion_factor - 1),
+                y + empty_cols_before * (expansion_factor - 1),
             )
         })
         .collect()
@@ -54,8 +56,7 @@ fn manhattan_distance(a: &Position, b: &Position) -> usize {
     x_diff + y_diff
 }
 
-fn part_1(lines: Lines) -> usize {
-    let galaxies: HashSet<Position> = expand_universe(parse_galaxies(lines));
+fn sum_of_shortest_paths_between_galaxies(galaxies: HashSet<(usize, usize)>) -> usize {
     let mut done: HashSet<Position> = HashSet::new();
     galaxies
         .iter()
@@ -69,8 +70,16 @@ fn part_1(lines: Lines) -> usize {
         .sum()
 }
 
-fn part_2(_lines: Lines) -> usize {
-    0
+fn part_1(lines: Lines) -> usize {
+    let expansion_factor = 2;
+    let galaxies = expand_universe(parse_galaxies(lines), expansion_factor);
+    sum_of_shortest_paths_between_galaxies(galaxies)
+}
+
+fn part_2(lines: Lines) -> usize {
+    let expansion_factor = 1_000_000;
+    let galaxies = expand_universe(parse_galaxies(lines), expansion_factor);
+    sum_of_shortest_paths_between_galaxies(galaxies)
 }
 
 pub fn solve() -> SolutionPair {
@@ -106,8 +115,19 @@ mod tests {
     }
 
     #[test]
-    fn test_part_2_example() {
-        assert_eq!(part_2(EXAMPLE_INPUT_1.lines()), 0);
+    fn test_part_2_example_expand_by_10() {
+        let expansion_factor = 10;
+        let galaxies = expand_universe(parse_galaxies(EXAMPLE_INPUT_1.lines()), expansion_factor);
+        println!("{:?}", galaxies);
+        assert_eq!(sum_of_shortest_paths_between_galaxies(galaxies), 1030);
+    }
+
+    #[test]
+    fn test_part_2_example_expand_by_100() {
+        let expansion_factor = 100;
+        let galaxies = expand_universe(parse_galaxies(EXAMPLE_INPUT_1.lines()), expansion_factor);
+        println!("{:?}", galaxies);
+        assert_eq!(sum_of_shortest_paths_between_galaxies(galaxies), 8410);
     }
 
     #[test]

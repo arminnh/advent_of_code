df5ec100b54b0a50ec43c945a6370df6ef75255c
Armin <haliovic.armin@hotmail.com>
2023-12-13 21:15:11 +0100

    Day 12 part 1 cleanup

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -4,7 +4,7 @@ use std::fmt::{Debug, Formatter, Result};
 use std::str::Lines;
 use std::usize;
 
-#[derive(PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 enum Status {
     Operational,
     Damaged,
@@ -36,28 +36,6 @@ impl Debug for Status {
     }
 }
 
-// impl Display for Candidate {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-//         let result = self.0.iter().fold(Ok(()), |result, status| {
-//             result.and_then(|_| write!(f, "{}", status))
-//         });
-
-//         result.and_then(|_| write!(f, " | "));
-
-//         self.1.iter().fold(result, |result, status| {
-//             result.and_then(|_| write!(f, "{}", status))
-//         })
-//     }
-// }
-
-// impl Display for Candidates {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-//         self.0.iter().fold(Ok(()), |result, candidate| {
-//             result.and_then(|_| write!(f, "{}", candidate))
-//         })
-//     }
-// }
-
 fn parse_line(line: &str) -> (Vec<Status>, Vec<usize>) {
     match line.split_ascii_whitespace().collect::<Vec<&str>>()[..] {
         [springs, damaged_spring_groups] => (
@@ -71,115 +49,58 @@ fn parse_line(line: &str) -> (Vec<Status>, Vec<usize>) {
     }
 }
 
-// count how many possible arrangements of operational and damaged springs fit the given criteria in each row.
-// contiguous groups of damaged springs are always separated by at least one operational spring
+fn valid_arrangement(candidate: &Vec<Status>, damaged_spring_groups: &Vec<usize>) -> bool {
+    candidate
+        .split(|x| *x == Status::Operational || *x == Status::Unknown)
+        .filter_map(|s| if s.len() > 0 { Some(s.len()) } else { None })
+        .collect::<Vec<usize>>()
+        .eq(damaged_spring_groups)
+}
+
+// Count how many possible arrangements of operational and damaged springs fit the given criteria in each row.
+// Contiguous groups of damaged springs are always separated by at least one operational spring.
 fn count_possible_arrangements(
     mut springs: Vec<Status>,
     damaged_spring_groups: Vec<usize>,
 ) -> usize {
-    // wrap the first input in Operational to avoid edge case checking
+    // Wrap the first input in operational to avoid edge case checking
     springs.insert(0, Status::Operational);
     springs.push(Status::Operational);
-    // for each candidate, keep track of how far along is has been evaluated
+    // For each candidate, keep track of how far along it has been evaluated
     let mut candidates: Vec<(Vec<Status>, usize)> = Vec::from([(springs, 1)]);
 
+    // Evolve the list of candidates for each group of damaged springs
     for group_size in damaged_spring_groups.iter() {
         let mut new_candidates = Vec::new();
         // println!("\nGroup {:?}\nCandidates:", group_size);
         // candidates.iter().for_each(|c| println!("  {:?}", c));
 
         while let Some((mut candidate, processed)) = candidates.pop() {
-            // iterate in window of group size + 1, because groups must be surrounded by operational springs ('.')
-            // println!(
-            //     "Candidate: {:?} | {:?}",
-            //     &candidate[0..processed],
-            //     &candidate[processed..]
-            // );
-
+            // println!("Candidate: {:?}, {:?}", candidate, processed);
             for i in processed..candidate.len() - group_size {
+                // Step in window of group size + 2. groups must be surrounded by operational springs.
                 let window = candidate.get(i - 1..i + group_size + 1).unwrap();
                 // println!("    window {:?}", window);
 
-                // if we can place damaged group here, then place it and add as new candidate for next iteration
+                // If damaged group fits here, then place it and add as new candidate for next group checking
                 if window.starts_with(&[Status::Operational])
                     && !window.ends_with(&[Status::Damaged])
                     && !window[1..1 + group_size].contains(&Status::Operational)
                 {
                     let mut new_candidate = candidate.clone();
-                    let mut new_group = Vec::new();
-                    for _ in 0..*group_size {
-                        new_group.push(Status::Damaged)
-                    }
+                    let mut new_group = vec![Status::Damaged].repeat(*group_size);
                     new_group.push(Status::Operational);
                     new_candidate.splice(i..i + group_size + 1, new_group);
                     // println!("     => Pushing new candidate {:?}", new_candidate);
                     new_candidates.push((new_candidate, i + group_size + 1));
                 }
 
+                // Moving on to evaluate later positions in the list, so
+                // change unknown at current position to operational for those candidates
                 if candidate[i] == Status::Unknown {
                     let _ = std::mem::replace(&mut candidate[i], Status::Operational);
                 }
             }
-
-            // evaluating
-            //     .clone()
-            //     .windows(group_size + 2)
-            //     .for_each(|window| {
-            //         println!("    window {:?}", window);
-
-            //         // if we can place damaged group here, then place it and add as new candidate for next iteration
-            //         if !window.starts_with(&[Status::Damaged])
-            //             && !window.ends_with(&[Status::Damaged])
-            //             && !window[1..window.len() - 1].contains(&Status::Operational)
-            //         {
-            //             let mut new_done: Vec<Status> = done.clone();
-            //             new_done.push(Status::Operational);
-            //             for _ in 0..*group_size {
-            //                 new_done.push(Status::Damaged);
-            //             }
-            //             new_done.push(Status::Operational);
-
-            //             let new_evaluating = evaluating
-            //                 .get(group_size + 2..evaluating.len())
-            //                 .unwrap()
-            //                 .to_vec();
-            //             println!(
-            //                 "     => Pushing new candidate {:?}, {:?}",
-            //                 new_done, new_evaluating
-            //             );
-            //             new_candidates.push((new_done, new_evaluating));
-            //         }
-
-            //         done.push(match evaluating.first().unwrap() {
-            //             Status::Operational | Status::Unknown => Status::Operational,
-            //             Status::Damaged => Status::Damaged,
-            //         });
-            //         evaluating.remove(0);
-            //     });
-
-            // for i in start_index..=candidate.len() - group_size - 2 {
-            //     // iterate in window of group size + 2, because groups must be surrounded by operational springs ('.')
-            //     let window = &candidate[i..i + group_size + 2];
-            //     println!("Checking window {:?} in candidate {:?}", window, candidate);
-
-            //     // if we can place damaged group here, then place it and add as new candidate for next iteration
-            //     if !window.starts_with(&[Status::Damaged])
-            //         && !window.ends_with(&[Status::Damaged])
-            //         && !window[1..window.len() - 1].contains(&Status::Operational)
-            //     {
-            //         let new_candidate = candidate[0..i + 1].to_string()
-            //             + &window[1..window.len() - 1].replace("?", "#")
-            //             + &window[window.len() - 1..window.len() - 1].replace("?", ".")
-            //             + &candidate[i + group_size + 2 - 1..];
-            //         println!("==> Pushing new candidate {:?}", new_candidate);
-            //         new_candidates.push((new_candidate, i + 1));
-            //     }
-
-            //     // if
-            //     if candidate.get(i + 1..i + 2).unwrap() == "?" {
-            //         candidate.replace_range(i + 1..i + 2, ".");
-            //     }
-            // }
         }
 
         candidates = new_candidates;
@@ -191,14 +112,6 @@ fn count_possible_arrangements(
         .iter()
         .filter(|(c, _)| valid_arrangement(&c, &damaged_spring_groups))
         .count()
-}
-
-fn valid_arrangement(candidate: &Vec<Status>, damaged_spring_groups: &Vec<usize>) -> bool {
-    candidate
-        .split(|x| *x == Status::Operational || *x == Status::Unknown)
-        .filter_map(|s| if s.len() > 0 { Some(s.len()) } else { None })
-        .collect::<Vec<usize>>()
-        .eq(damaged_spring_groups)
 }
 
 fn part_1(lines: Lines) -> usize {

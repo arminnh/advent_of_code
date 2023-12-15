d4353aad8c76a10f381530bb1d6bf196e19d1f55
Armin <haliovic.armin@hotmail.com>
2023-12-15 15:55:39 +0100

    day 15 part 1

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -3,8 +3,22 @@ use crate::{Solution, SolutionPair};
 use std::str::Lines;
 use std::usize;
 
-fn part_1(_lines: Lines) -> usize {
-    0
+fn hash(s: &str) -> usize {
+    let mut current = 0;
+
+    s.chars().for_each(|c| {
+        current += c as usize;
+        current *= 17;
+        current %= 256;
+    });
+
+    current
+}
+
+fn part_1(lines: Lines) -> usize {
+    lines
+        .map(|line| line.split(',').map(|s| hash(s)).sum::<usize>())
+        .sum()
 }
 
 fn part_2(_lines: Lines) -> usize {
@@ -12,9 +26,10 @@ fn part_2(_lines: Lines) -> usize {
 }
 
 pub fn solve() -> SolutionPair {
+    let input = load_input("inputs/day_11");
     (
-        Solution::from(part_1(load_input("inputs/day_15").lines())),
-        Solution::from(part_2(load_input("inputs/day_15").lines())),
+        Solution::from(part_1(input.lines())),
+        Solution::from(part_2(input.lines())),
     )
 }
 
@@ -22,25 +37,34 @@ pub fn solve() -> SolutionPair {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT_1: &str = "";
+    const EXAMPLE_INPUT_1: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+    #[test]
+    fn test_hash() {
+        assert_eq!(hash("rn=1"), 30);
+        assert_eq!(hash("cm-"), 253);
+        assert_eq!(hash("pc=4"), 180);
+        assert_eq!(hash("ot=9"), 9);
+        assert_eq!(hash("ot=7"), 231);
+    }
 
     #[test]
     fn test_part_1_example() {
-        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 1320);
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(load_input("inputs/day_15").lines()), 0);
+        assert_eq!(part_1(load_input("inputs/day_15").lines()), 513158);
     }
 
-    #[test]
-    fn test_part_2_example() {
-        assert_eq!(part_2(EXAMPLE_INPUT_1.lines()), 0);
-    }
+    // #[test]
+    // fn test_part_2_example() {
+    //     assert_eq!(part_2(EXAMPLE_INPUT_1.lines()), 0);
+    // }
 
-    #[test]
-    fn test_part_2() {
-        assert_eq!(part_2(load_input("inputs/day_15").lines()), 0);
-    }
+    // #[test]
+    // fn test_part_2() {
+    //     assert_eq!(part_2(load_input("inputs/day_15").lines()), 0);
+    // }
 }

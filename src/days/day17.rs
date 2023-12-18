8e5347cf5ee7c0b58ba4b8faa2695618376ce0ca
Armin <haliovic.armin@hotmail.com>
2023-12-18 09:06:06 +0100

    day 17 part 1 WIP

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,10 +1,186 @@
 use crate::days::util::load_input;
 use crate::{Solution, SolutionPair};
+use std::collections::{BinaryHeap, HashSet};
 use std::str::Lines;
 use std::usize;
 
-fn part_1(_lines: Lines) -> usize {
-    0
+struct Node {
+    cost: usize,
+    total_cost: usize,
+    previous: Option<(usize, usize)>,
+}
+
+fn parse_grid(lines: Lines<'_>) -> Vec<Vec<Node>> {
+    lines
+        .map(|line| {
+            line.chars()
+                .map(|c| Node {
+                    cost: c as usize - 48,
+                    total_cost: usize::MAX,
+                    previous: None,
+                })
+                .collect::<Vec<Node>>()
+        })
+        .collect()
+}
+
+fn print_path(grid: &Vec<Vec<Node>>) {
+    let mut path: HashSet<(usize, usize)> = HashSet::new();
+    let mut current = grid.last().unwrap().last().unwrap();
+    while let Some(pos) = current.previous {
+        if path.insert(pos) {
+            current = &grid[pos.0][pos.1];
+        } else {
+            break;
+        }
+    }
+    grid.iter().enumerate().for_each(|(x, row)| {
+        row.iter().enumerate().for_each(|(y, node)| {
+            if path.contains(&(x, y)) {
+                print!("•");
+            } else {
+                print!("{}", node.cost);
+            }
+        });
+        println!();
+    });
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    // Return the next position after moving in the direction.
+    fn apply_to_pos(
+        &self,
+        pos: (usize, usize),
+        max_x: usize,
+        max_y: usize,
+    ) -> Option<(usize, usize)> {
+        match self {
+            Direction::Up if pos.0 > 0 => Some((pos.0 - 1, pos.1)),
+            Direction::Down if pos.0 < max_x - 1 => Some((pos.0 + 1, pos.1)),
+            Direction::Left if pos.1 > 0 => Some((pos.0, pos.1 - 1)),
+            Direction::Right if pos.1 < max_y - 1 => Some((pos.0, pos.1 + 1)),
+            _ => None,
+        }
+    }
+
+    // Cannot turn back, so return the other three directions.
+    fn next_directions(&self) -> [Self; 3] {
+        match self {
+            Direction::Up => [Direction::Up, Direction::Left, Direction::Right],
+            Direction::Down => [Direction::Down, Direction::Left, Direction::Right],
+            Direction::Left => [Direction::Up, Direction::Down, Direction::Left],
+            Direction::Right => [Direction::Up, Direction::Down, Direction::Right],
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct SearchStep {
+    cost: usize,
+    position: (usize, usize),
+    direction: Direction,
+    steps_in_direction: i32,
+}
+
+impl SearchStep {
+    fn new(
+        cost: usize,
+        position: (usize, usize),
+        direction: Direction,
+        steps_in_direction: i32,
+    ) -> Self {
+        Self {
+            cost,
+            position,
+            direction,
+            steps_in_direction,
+        }
+    }
+
+    fn next(
+        &self,
+        direction: Direction,
+        grid: &Vec<Vec<Node>>,
+        max_x: usize,
+        max_y: usize,
+    ) -> Option<Self> {
+        let position = direction.apply_to_pos(self.position, max_x, max_y)?;
+        let cost = self.cost + grid[position.0][position.1].cost;
+
+        if self.direction == direction {
+            if self.steps_in_direction < 3 {
+                Some(SearchStep::new(
+                    cost,
+                    position,
+                    direction,
+                    self.steps_in_direction + 1,
+                ))
+            } else {
+                None
+            }
+        } else {
+            Some(SearchStep::new(cost, position, direction, 1))
+        }
+    }
+}
+
+impl Ord for SearchStep {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // BinaryHeap is a max-heap so reverse the comparison
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for SearchStep {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn part_1(lines: Lines) -> usize {
+    let mut grid = parse_grid(lines);
+    let max_x = grid.len();
+    let max_y = grid[0].len();
+    let mut visited: HashSet<((usize, usize), Direction)> = HashSet::new();
+    let mut frontier: BinaryHeap<SearchStep> =
+        BinaryHeap::from([SearchStep::new(0, (0, 0), Direction::Down, -1)]);
+
+    while let Some(current) = frontier.pop() {
+        // println!("{:?}", current);
+        if current.position == (max_x - 1, max_y - 1) {
+            print_path(&grid);
+            return current.cost;
+        }
+
+        if !visited.insert((current.position, current.direction)) {
+            continue;
+        }
+
+        for direction in current.direction.next_directions() {
+            if let Some(next) = current.next(direction, &grid, max_x, max_y) {
+                let node = &mut grid[next.position.0][next.position.1];
+                if next.cost < node.total_cost {
+                    // println!(
+                    //     "    {:?} <- {:?}, {:?}",
+                    //     next.position, current.position, next.cost
+                    // );
+                    node.total_cost = next.cost;
+                    node.previous = Some(current.position);
+                }
+                frontier.push(next);
+            }
+        }
+    }
+
+    panic!("Oh no!")
 }
 
 fn part_2(_lines: Lines) -> usize {
@@ -12,9 +188,10 @@ fn part_2(_lines: Lines) -> usize {
 }
 
 pub fn solve() -> SolutionPair {
+    let input = load_input("inputs/day_17");
     (
-        Solution::from(part_1(load_input("inputs/day_17").lines())),
-        Solution::from(part_2(load_input("inputs/day_17").lines())),
+        Solution::from(part_1(input.lines())),
+        Solution::from(part_2(input.lines())),
     )
 }
 
@@ -22,11 +199,23 @@ pub fn solve() -> SolutionPair {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT_1: &str = "";
+    const EXAMPLE_INPUT: &str = "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
 
     #[test]
     fn test_part_1_example() {
-        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT.lines()), 102);
     }
 
     #[test]
@@ -36,7 +225,7 @@ mod tests {
 
     #[test]
     fn test_part_2_example() {
-        assert_eq!(part_2(EXAMPLE_INPUT_1.lines()), 0);
+        assert_eq!(part_2(EXAMPLE_INPUT.lines()), 0);
     }
 
     #[test]

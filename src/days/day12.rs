4c40107a858bdf5ad8375406bef8642aaba790c9
Armin <haliovic.armin@hotmail.com>
2023-12-26 22:25:21 +0100

    day 12 recursive version with caching

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,45 +1,45 @@
 use crate::days::util::load_input;
 use crate::{Solution, SolutionPair};
+use std::collections::HashMap;
 use std::fmt::{Debug, Formatter, Result};
 use std::str::Lines;
 use std::usize;
 
-#[derive(PartialEq, Eq, Copy, Clone)]
-enum Status {
+#[derive(PartialEq, Eq, Copy, Clone, Hash)]
+enum Spring {
     Operational,
     Damaged,
     Unknown,
 }
 
-impl Status {
-    fn from_char(c: char) -> Self {
+impl From<char> for Spring {
+    fn from(c: char) -> Self {
         match c {
-            '.' => Status::Operational,
-            '#' => Status::Damaged,
-            '?' => Status::Unknown,
-            _ => panic!("Invalid character {:?}", c),
+            '.' => Spring::Operational,
+            '#' => Spring::Damaged,
+            _ => Spring::Unknown,
         }
     }
 }
 
-impl Debug for Status {
+impl Debug for Spring {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(
             f,
             "{}",
             match self {
-                Status::Operational => '.',
-                Status::Damaged => '#',
-                Status::Unknown => '?',
+                Spring::Operational => '.',
+                Spring::Damaged => '#',
+                Spring::Unknown => '?',
             }
         )
     }
 }
 
-fn parse_line(line: &str) -> (Vec<Status>, Vec<usize>) {
+fn parse_line(line: &str) -> (Vec<Spring>, Vec<usize>) {
     match line.split_ascii_whitespace().collect::<Vec<&str>>()[..] {
         [springs, damaged_spring_groups] => (
-            springs.chars().map(|c| Status::from_char(c)).collect(),
+            springs.chars().map(|c| Spring::from(c)).collect(),
             damaged_spring_groups
                 .split(',')
                 .map(|num| num.parse::<usize>().unwrap())
@@ -51,81 +51,110 @@ fn parse_line(line: &str) -> (Vec<Status>, Vec<usize>) {
 
 // Count how many possible arrangements of operational and damaged springs fit the given criteria in each row.
 // Contiguous groups of damaged springs are always separated by at least one operational spring.
-fn count_possible_arrangements(
-    mut springs: Vec<Status>,
+fn possible_arrangements(
+    springs: Vec<Spring>,
     damaged_spring_groups: Vec<usize>,
+    cache: &mut HashMap<(Vec<Spring>, Vec<usize>), usize>,
 ) -> usize {
-    // Wrap the first input in operational to avoid edge case checking
-    springs.insert(0, Status::Operational);
-    springs.push(Status::Operational);
-    // For each candidate, keep track of how far along it has been evaluated
-    let mut candidates: Vec<(Vec<Status>, usize)> = Vec::from([(springs, 1)]);
-
-    // Evolve the list of candidates for each group of damaged springs
-    for (group_index, group_size) in damaged_spring_groups.iter().enumerate() {
-        let mut new_candidates = Vec::new();
-
-        while let Some((mut candidate, processed)) = candidates.pop() {
-            for i in processed..candidate.len() - group_size {
-                // Step through in windows of group size + 2, since groups must be surrounded by operational springs
-                let window = candidate.get(i - 1..i + group_size + 1).unwrap();
-
-                if window.starts_with(&[Status::Damaged]) {
-                    // Just moved over a damaged group larger than current group size, so stop here for this group
-                    break;
-                }
-
-                // If damaged group fits here, then place it and add as new candidate for next group checking
-                if !window.ends_with(&[Status::Damaged])
-                    && !window[1..1 + group_size].contains(&Status::Operational)
-                {
-                    let mut new_candidate = candidate.clone();
-                    let mut new_group = vec![Status::Damaged].repeat(*group_size);
-                    new_group.push(Status::Operational);
-                    new_candidate.splice(i..i + group_size + 1, new_group);
-
-                    // When checking last group, the rest of the candidate cannot contain damaged springs
-                    if group_index < damaged_spring_groups.len() - 1
-                        || !new_candidate
-                            .get(i + group_size + 1..)
-                            .unwrap()
-                            .contains(&Status::Damaged)
-                    {
-                        new_candidates.push((new_candidate, i + group_size + 1));
-                    }
-                }
-
-                // Moving on to evaluate later positions in the list, so
-                // change unknown at current position to operational for those candidates
-                if candidate[i] == Status::Unknown {
-                    let _ = std::mem::replace(&mut candidate[i], Status::Operational);
-                }
-            }
-        }
-
-        candidates = new_candidates;
+    // println!("{springs:?}, {damaged_spring_groups:?}");
+    if let Some(result) = cache.get(&(springs.clone(), damaged_spring_groups.clone())) {
+        // println!("    Hit {result:?}");
+        return *result;
     }
 
-    candidates.len()
+    if let Some(&group_size) = damaged_spring_groups.first() {
+        // If no space left for remaining groups (+ buffers for operational springs) -> no match
+        if springs.len()
+            < damaged_spring_groups.iter().sum::<usize>() + damaged_spring_groups.len() - 1
+        {
+            // println!("    no room left");
+            cache.insert((springs, damaged_spring_groups), 0);
+            return 0;
+        }
+
+        // If first spring is operational, nothing to do here -> skip all operational springs
+        if springs[0] == Spring::Operational {
+            // println!("    skip Operational");
+            let result =
+                possible_arrangements(springs[1..].to_vec(), damaged_spring_groups.clone(), cache);
+            cache.insert((springs, damaged_spring_groups), result);
+            return result;
+        }
+
+        // Starting from the first spring (+ 1 for the operational spring after the group),
+        // if a damaged group fits, then "place" it and check the result for the rest of the springs
+        let mut result = 0;
+        if !springs[..group_size].contains(&Spring::Operational)
+            && (springs.len() == group_size || springs[group_size] != Spring::Damaged)
+        {
+            // println!("    place group");
+            result = possible_arrangements(
+                springs[std::cmp::min(group_size + 1, springs.len())..].to_vec(),
+                damaged_spring_groups[1..].to_vec(),
+                cache,
+            );
+        }
+
+        // If first spring is unknown, add the number of results if we were to skip it
+        // instead of placing a potential damaged group
+        if springs[0] == Spring::Unknown {
+            // println!("    skip group");
+            result +=
+                possible_arrangements(springs[1..].to_vec(), damaged_spring_groups.clone(), cache);
+        }
+
+        cache.insert((springs, damaged_spring_groups), result);
+        result
+    } else {
+        // No groups left, but still have a damaged spring left -> no match
+        let result = match springs.contains(&Spring::Damaged) {
+            true => 0,
+            false => 1,
+        };
+        // println!("    no groups left -> {result}");
+        cache.insert((springs, damaged_spring_groups), result);
+        result
+    }
 }
 
 fn part_1(lines: Lines) -> usize {
+    let mut cache = HashMap::new();
     lines
         .map(|line| {
             let (springs, damaged_spring_groups) = parse_line(line);
-            count_possible_arrangements(springs, damaged_spring_groups)
+            possible_arrangements(springs, damaged_spring_groups, &mut cache)
         })
         .sum()
 }
 
-fn part_2(_lines: Lines) -> usize {
-    0
+fn unfold_input(
+    springs: Vec<Spring>,
+    damaged_spring_groups: Vec<usize>,
+) -> (Vec<Spring>, Vec<usize>) {
+    let mut new_springs = springs.clone();
+    for _ in 0..4 {
+        new_springs.push(Spring::Unknown);
+        new_springs.append(&mut springs.clone());
+    }
+    (new_springs, damaged_spring_groups.repeat(5))
+}
+
+fn part_2(lines: Lines) -> usize {
+    let mut cache = HashMap::new();
+    lines
+        .map(|line| {
+            let (springs, damaged_spring_groups) = parse_line(line);
+            let (springs, damaged_spring_groups) = unfold_input(springs, damaged_spring_groups);
+            possible_arrangements(springs, damaged_spring_groups, &mut cache)
+        })
+        .sum()
 }
 
 pub fn solve() -> SolutionPair {
+    let input = load_input("inputs/day_12");
     (
-        Solution::from(part_1(load_input("inputs/day_12").lines())),
-        Solution::from(part_2(load_input("inputs/day_12").lines())),
+        Solution::from(part_1(input.lines())),
+        Solution::from(part_2(input.lines())),
     )
 }
 
@@ -143,37 +172,78 @@ mod tests {
     #[test]
     fn test_count_possible_arrangements_1() {
         let (springs, groups) = parse_line("???.### 1,1,3");
-        assert_eq!(count_possible_arrangements(springs, groups), 1);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
     }
 
     #[test]
     fn test_count_possible_arrangements_2() {
         let (springs, groups) = parse_line(".??..??...?##. 1,1,3");
-        assert_eq!(count_possible_arrangements(springs, groups), 4);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 4);
     }
 
     #[test]
     fn test_count_possible_arrangements_3() {
         let (springs, groups) = parse_line("?#?#?#?#?#?#?#? 1,3,1,6");
-        assert_eq!(count_possible_arrangements(springs, groups), 1);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
     }
 
     #[test]
     fn test_count_possible_arrangements_4() {
         let (springs, groups) = parse_line("????.#...#... 4,1,1");
-        assert_eq!(count_possible_arrangements(springs, groups), 1);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
     }
 
     #[test]
     fn test_count_possible_arrangements_5() {
         let (springs, groups) = parse_line("????.######..#####. 1,6,5");
-        assert_eq!(count_possible_arrangements(springs, groups), 4);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 4);
     }
 
     #[test]
     fn test_count_possible_arrangements_6() {
         let (springs, groups) = parse_line("?###???????? 3,2,1");
-        assert_eq!(count_possible_arrangements(springs, groups), 10);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 10);
+    }
+
+    #[test]
+    fn test_count_possible_arrangements_7() {
+        let (springs, groups) = parse_line("#????##????#?#??#?? 1,1,13");
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 3);
+    }
+
+    #[test]
+    fn test_count_possible_arrangements_8() {
+        let (springs, groups) = parse_line("?.#?#??#?#. 1,6,1");
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
+    }
+
+    #[test]
+    fn test_count_possible_arrangements_9() {
+        let (springs, groups) = parse_line(".#?.???????????#..? 2,5,1,1,1,1");
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 4);
+    }
+
+    #[test]
+    fn test_count_possible_arrangements_10() {
+        let (springs, groups) = parse_line("??..???.?#????????? 1,3,2,1,1,1");
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 60);
+    }
+
+    #[test]
+    fn test_count_possible_arrangements_11() {
+        let (springs, groups) = parse_line("??????#?#? 1,1,3");
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 6);
     }
 
     #[test]
@@ -182,47 +252,65 @@ mod tests {
     }
 
     #[test]
-    fn test_count_possible_arrangements_7() {
-        let (springs, groups) = parse_line("#????##????#?#??#?? 1,1,13");
-        assert_eq!(count_possible_arrangements(springs, groups), 3);
-    }
-
-    #[test]
-    fn test_count_possible_arrangements_8() {
-        let (springs, groups) = parse_line("?.#?#??#?#. 1,6,1");
-        assert_eq!(count_possible_arrangements(springs, groups), 1);
-    }
-
-    #[test]
-    fn test_count_possible_arrangements_9() {
-        let (springs, groups) = parse_line(".#?.???????????#..? 2,5,1,1,1,1");
-        assert_eq!(count_possible_arrangements(springs, groups), 4);
-    }
-
-    #[test]
-    fn test_count_possible_arrangements_10() {
-        let (springs, groups) = parse_line("??..???.?#????????? 1,3,2,1,1,1");
-        assert_eq!(count_possible_arrangements(springs, groups), 60);
-    }
-
-    #[test]
-    fn test_count_possible_arrangements_11() {
-        let (springs, groups) = parse_line("??????#?#? 1,1,3");
-        assert_eq!(count_possible_arrangements(springs, groups), 6);
-    }
-
-    #[test]
     fn test_part_1() {
         assert_eq!(part_1(load_input("inputs/day_12").lines()), 7379);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT.lines()), 0);
-    // }
+    #[test]
+    fn test_part_2_example_line_1() {
+        let (springs, groups) = parse_line("???.### 1,1,3");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
+    }
 
-    // #[test]
-    // fn test_part_2() {
-    //     assert_eq!(part_2(load_input("inputs/day_12").lines()), 0);
-    // }
+    #[test]
+    fn test_part_2_example_line_2() {
+        let (springs, groups) = parse_line(".??..??...?##. 1,1,3");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 16384);
+    }
+
+    #[test]
+    fn test_part_2_example_line_3() {
+        let (springs, groups) = parse_line("?#?#?#?#?#?#?#? 1,3,1,6");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 1);
+    }
+
+    #[test]
+    fn test_part_2_example_line_4() {
+        let (springs, groups) = parse_line("????.#...#... 4,1,1");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 16);
+    }
+
+    #[test]
+    fn test_part_2_example_line_5() {
+        let (springs, groups) = parse_line("????.######..#####. 1,6,5");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 2500);
+    }
+
+    #[test]
+    fn test_part_2_example_line_6() {
+        let (springs, groups) = parse_line("?###???????? 3,2,1");
+        let (springs, groups) = unfold_input(springs, groups);
+        let mut cache = HashMap::new();
+        assert_eq!(possible_arrangements(springs, groups, &mut cache), 506250);
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT.lines()), 525152);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(load_input("inputs/day_12").lines()), 7732028747925);
+    }
 }

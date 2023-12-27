b006b7b4460023d3cb4eedccf18fd11e231630b9
Armin <haliovic.armin@hotmail.com>
2023-12-27 13:06:29 +0100

    day 13 part 2

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -39,11 +39,35 @@ fn transposed(grid: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     transposed
 }
 
-fn reflecting_row(grid: &Vec<Vec<bool>>) -> Option<usize> {
+fn nr_of_differences<T>(left: &[T], right: &[T]) -> usize
+where
+    T: Eq,
+{
+    left.iter()
+        .zip(right.iter())
+        .filter(|(l, r)| l != r)
+        .count()
+}
+
+// Find the reflecting row in the grid while cleaning the given number of smudges.
+fn reflecting_row(grid: &Vec<Vec<bool>>, smudges: usize) -> Option<usize> {
     for (i, rows) in grid.windows(2).enumerate() {
-        if rows[0] == rows[1] {
+        let mut differences = nr_of_differences(&rows[0], &rows[1]);
+        if differences <= smudges {
             // row i and i+1 reflect -> check if reflection goes back to one of the edges
-            if (1..=i).all(|x| i + 1 + x >= grid.len() || grid[i - x] == grid[i + 1 + x]) {
+            for x in 1..=i {
+                if i + 1 + x >= grid.len() {
+                    // reached edge of grid
+                    break;
+                }
+                differences += nr_of_differences(&grid[i - x], &grid[i + 1 + x]);
+                if differences > smudges {
+                    // early termination in case there's already too many differences
+                    break;
+                }
+            }
+            // Cleaning the exact amount of smudges, not <=
+            if differences == smudges {
                 return Some(i);
             }
         }
@@ -52,38 +76,42 @@ fn reflecting_row(grid: &Vec<Vec<bool>>) -> Option<usize> {
     None
 }
 
-fn reflection_score(mut grid: Vec<Vec<bool>>) -> usize {
-    if let Some(r) = reflecting_row(&grid) {
+// The reflection score of a grid is either the reflecting row * 100 or the reflecting column.
+fn reflection_score(grid: Vec<Vec<bool>>, smudges: usize) -> usize {
+    if let Some(r) = reflecting_row(&grid, smudges) {
         return (r + 1) * 100;
     }
 
     let transposed: Vec<Vec<bool>> = transposed(grid);
 
-    if let Some(r) = reflecting_row(&transposed) {
+    if let Some(r) = reflecting_row(&transposed, smudges) {
         return r + 1;
     }
 
-    panic!("No solution!");
-}
-
-fn part_1(input: String) -> usize {
-    input
-        .split("\n\n")
-        .map(|pattern| {
-            let grid: Vec<Vec<bool>> = parse_grid(pattern);
-            reflection_score(grid)
-        })
-        .sum()
-}
-
-fn part_2(lines: String) -> usize {
     0
 }
 
+fn part_1(input: String) -> usize {
+    let smudges = 0;
+    input
+        .split("\n\n")
+        .map(|pattern| reflection_score(parse_grid(pattern), smudges))
+        .sum()
+}
+
+fn part_2(input: String) -> usize {
+    let smudges = 1;
+    input
+        .split("\n\n")
+        .map(|pattern| reflection_score(parse_grid(pattern), smudges))
+        .sum()
+}
+
 pub fn solve() -> SolutionPair {
+    let input = load_input("inputs/day_13");
     (
-        Solution::from(part_1(load_input("inputs/day_13"))),
-        Solution::from(part_2(load_input("inputs/day_13"))),
+        Solution::from(part_1(input.clone())),
+        Solution::from(part_2(input)),
     )
 }
 
@@ -107,28 +135,100 @@ mod tests {
 ..##..###
 #....#..#";
 
-    #[test]
-    fn test_reflection_score_vertical() {
-        let grid = "#.##..##.
+    const GRID_1: &str = "#.##..##.
 ..#.##.#.
 ##......#
 ##......#
 ..#.##.#.
 ..##..##.
 #.#.##.#.";
-        assert_eq!(reflection_score(parse_grid(grid)), 5);
-    }
 
-    #[test]
-    fn test_reflection_score_horizontal() {
-        let grid = "#...##..#
+    const GRID_2: &str = "#...##..#
 #....#..#
 ..##..###
 #####.##.
 #####.##.
 ..##..###
 #....#..#";
-        assert_eq!(reflection_score(parse_grid(grid)), 400);
+
+    const GRID_3: &str = ".#.##.#.#
+.##..##..
+.#.##.#..
+#......##
+#......##
+.#.##.#..
+.##..##.#";
+
+    const GRID_4: &str = "#..#....#
+###..##..
+.##.#####
+.##.#####
+###..##..
+#..#....#
+#..##...#";
+
+    const GRID_5: &str = "#.##..##.
+..#.##.#.
+##..#...#
+##...#..#
+..#.##.#.
+..##..##.
+#.#.##.#.";
+
+    const GRID_6: &str = "###.##.##
+##.####.#
+##.#..#.#
+####..###
+....##...
+##.#..#.#
+...#..#..
+##..###.#
+##......#
+##......#
+..#.##.#.
+...#..#..
+##.####.#
+....##...
+...####..
+....##...
+##.####.#";
+
+    const GRID_7: &str = ".##.##...##...##.
+#####..##..##..##
+.....##..##..##..
+.##.#.#.####.#.#.
+.##...#.#..#.#...
+....#..........#.
+#..#..#......#..#
+....###.....####.
+.##...#.#..#.#...
+.....#..####..#..
+#..#...##..##...#
+....#...#..#...#.
+#..#.##########.#
+#..##...####...##
+#####.##.##.##.##";
+
+    #[test]
+    fn test_reflection_score() {
+        assert_eq!(reflection_score(parse_grid(GRID_1), 0), 5);
+        assert_eq!(reflection_score(parse_grid(GRID_2), 0), 400);
+        assert_eq!(reflection_score(parse_grid(GRID_3), 0), 4);
+        assert_eq!(reflection_score(parse_grid(GRID_4), 0), 300);
+        assert_eq!(reflection_score(parse_grid(GRID_5), 0), 0);
+        assert_eq!(reflection_score(parse_grid(GRID_6), 0), 1);
+        assert_eq!(reflection_score(parse_grid(GRID_7), 0), 2);
+    }
+
+    #[test]
+    fn test_reflection_score_tolerance() {
+        assert_eq!(reflection_score(parse_grid(GRID_1), 1), 300);
+        assert_eq!(reflection_score(parse_grid(GRID_2), 1), 100);
+        assert_eq!(reflection_score(parse_grid(GRID_3), 0), 4);
+        assert_eq!(reflection_score(parse_grid(GRID_4), 0), 300);
+        assert_eq!(reflection_score(parse_grid(GRID_5), 0), 0);
+        assert_eq!(reflection_score(parse_grid(GRID_6), 1), 5);
+        assert_eq!(reflection_score(parse_grid(GRID_7), 1), 10);
     }
 
     #[test]
@@ -141,13 +241,13 @@ mod tests {
         assert_eq!(part_1(load_input("inputs/day_13")), 34100);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT), 0);
-    // }
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT.to_string()), 400);
+    }
 
-    // #[test]
-    // fn test_part_2() {
-    //     assert_eq!(part_2(load_input("inputs/day_13")), 0);
-    // }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(load_input("inputs/day_13")), 33106);
+    }
 }

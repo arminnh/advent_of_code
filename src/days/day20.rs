6d954f946833210cdada38a9f86f3271f8803502
Armin <haliovic.armin@hotmail.com>
2024-01-05 22:45:16 +0100

    day 20 part 1

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,12 +1,12 @@
 use crate::days::util::load_input;
 use crate::{Solution, SolutionPair};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fmt;
 use std::str::Lines;
 
 // Modules communicate using pulses. Each pulse is either a high pulse or a low pulse.
 // When a module sends a pulse, it sends that type of pulse to each module in its list of destination modules.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Pulse {
     High,
     Low,
@@ -20,7 +20,7 @@ trait CommunicationModule: fmt::Debug {
     // Modules communicate using pulses. Each pulse is either a high
     // pulse or a low pulse. When a module sends a pulse, it sends
     // that type of pulse to each module in its list of destination modules.
-    fn process_pulse(&mut self, module_id: usize, pulse: Pulse) -> Option<Pulse>;
+    fn process_pulse(&mut self, source_module_id: usize, pulse: Pulse) -> Option<Pulse>;
 
     fn register_input_module(&mut self, module_id: usize);
 }
@@ -92,8 +92,8 @@ impl Conjunction {
 impl CommunicationModule for Conjunction {
     // When a pulse is received, the conjunction module first updates its memory for that input.
     // Then, if it remembers high pulses for all inputs, it sends a low pulse; otherwise, it sends a high pulse.
-    fn process_pulse(&mut self, module_id: usize, pulse: Pulse) -> Option<Pulse> {
-        self.memory.insert(module_id, pulse);
+    fn process_pulse(&mut self, source_module_id: usize, pulse: Pulse) -> Option<Pulse> {
+        self.memory.insert(source_module_id, pulse);
 
         if self.memory.values().all(|p| *p == Pulse::High) {
             Some(Pulse::Low)
@@ -193,13 +193,57 @@ fn parse_input(
     (modules, module_destinations, broadcaster)
 }
 
-fn part_1(lines: Lines) -> usize {
-    let (modules, module_destinations, broadcaster) = parse_input(lines);
+// At Desert Machine Headquarters, there is a module with a single button on it called, aptly, the button module.
+// When you push the button, a single low pulse is sent directly to the broadcaster module.
+// After pushing the button, you must wait until all pulses have been delivered and fully handled
+// before pushing it again. Never push the button if modules are still processing pulses.
+// Returns the amounts of low and high pulses sent in total for the button press.
+fn handle_button_press(
+    modules: &mut Vec<Box<dyn CommunicationModule>>,
+    module_destinations: &Vec<Vec<usize>>,
+    broadcaster: &Broadcaster,
+) -> (usize, usize) {
+    let mut low_pulse_count = 1;
+    let mut high_pulse_count = 0;
+    // Each signal is pushed to the back of the signals queue to ensure correct order of processing
+    let mut signals: VecDeque<(usize, usize, Pulse)> = broadcaster
+        .iter()
+        .map(|&destination| (0, destination, Pulse::Low))
+        .collect();
 
-    dbg!(&modules);
-    dbg!(&module_destinations);
-    dbg!(&broadcaster);
-    0
+    while let Some((source, destination, pulse)) = signals.pop_front() {
+        // println!("{source} ---{pulse:?}--> {destination}");
+        match pulse {
+            Pulse::High => high_pulse_count += 1,
+            Pulse::Low => low_pulse_count += 1,
+        };
+
+        if let Some(new_pulse) = modules[destination].process_pulse(source, pulse) {
+            module_destinations[destination]
+                .iter()
+                .for_each(|&new_destination| {
+                    signals.push_back((destination, new_destination, new_pulse))
+                });
+        }
+    }
+
+    (low_pulse_count, high_pulse_count)
+}
+
+// Determine the number of low pulses and high pulses that would be sent after pushing the button 1000 times,
+// waiting for all pulses to be fully handled after each push of the button. What do you get if you multiply
+// the total number of low pulses sent by the total number of high pulses sent?
+fn part_1(lines: Lines) -> usize {
+    let (mut modules, module_destinations, broadcaster) = parse_input(lines);
+
+    // 1000 is low enough to brute force quickly
+    let (low_pulses, high_pulses) = (0..1000).fold((0, 0), |(low_count, high_count), _| {
+        let (low, high) = handle_button_press(&mut modules, &module_destinations, &broadcaster);
+        // println!("{}, {}", low_count + low, high_count + high);
+        (low_count + low, high_count + high)
+    });
+
+    low_pulses * high_pulses
 }
 
 fn part_2(_lines: Lines) -> usize {
@@ -232,17 +276,17 @@ mod tests {
 
     #[test]
     fn test_part_1_example_1() {
-        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 32_000_000);
     }
 
     #[test]
     fn test_part_1_example_2() {
-        assert_eq!(part_1(EXAMPLE_INPUT_2.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT_2.lines()), 11_687_500);
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(load_input("inputs/day_20").lines()), 0);
+        assert_eq!(part_1(load_input("inputs/day_20").lines()), 912_199_500);
     }
 
     #[test]

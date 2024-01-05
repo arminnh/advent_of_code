45b0dff4b1c0c4d744ae74acdbe846683a602e42
Armin <haliovic.armin@hotmail.com>
2024-01-05 21:51:55 +0100

    day 20 object soup initialization

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,15 +1,12 @@
 use crate::days::util::load_input;
 use crate::{Solution, SolutionPair};
-use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
-use std::rc::Rc;
+use std::fmt;
 use std::str::Lines;
-use std::usize;
 
 // Modules communicate using pulses. Each pulse is either a high pulse or a low pulse.
 // When a module sends a pulse, it sends that type of pulse to each module in its list of destination modules.
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq)]
 enum Pulse {
     High,
     Low,
@@ -19,17 +16,32 @@ enum Pulse {
 // The cables don't connect to the machines directly, but rather
 // to communication modules attached to the machines that perform
 // various initialization tasks and also act as communication relays.
-trait CommunicationModule {
+trait CommunicationModule: fmt::Debug {
     // Modules communicate using pulses. Each pulse is either a high
     // pulse or a low pulse. When a module sends a pulse, it sends
     // that type of pulse to each module in its list of destination modules.
-    fn process_pulse(&mut self, name: String, pulse: Pulse) -> Option<Pulse>;
+    fn process_pulse(&mut self, module_id: usize, pulse: Pulse) -> Option<Pulse>;
 
-    fn register_input_module(&mut self, name: String);
+    fn register_input_module(&mut self, module_id: usize);
 }
 
+// A sink has no state and does nothing with signals
+#[derive(Debug, PartialEq, Eq)]
+struct Sink {}
+
+impl CommunicationModule for Sink {
+    fn process_pulse(&mut self, _module_id: usize, _pulse: Pulse) -> Option<Pulse> {
+        None
+    }
+
+    fn register_input_module(&mut self, _: usize) {}
+}
+
+// When a broadcaster receives a pulse, it sends the same pulse to all of its destination modules.
+type Broadcaster = Vec<usize>;
+
 // Flip-flop modules are either on or off; they are initially off.
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq)]
 struct FlipFlop {
     on: bool,
 }
@@ -44,7 +56,7 @@ impl CommunicationModule for FlipFlop {
     // If a flip-flop module receives a high pulse, it is ignored and nothing happens.
     // However, if a flip-flop module receives a low pulse, it flips between on and off.
     // If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
-    fn process_pulse(&mut self, _name: String, pulse: Pulse) -> Option<Pulse> {
+    fn process_pulse(&mut self, _module_id: usize, pulse: Pulse) -> Option<Pulse> {
         match pulse {
             Pulse::High => None,
             Pulse::Low => {
@@ -59,14 +71,14 @@ impl CommunicationModule for FlipFlop {
         }
     }
 
-    fn register_input_module(&mut self, _name: String) {}
+    fn register_input_module(&mut self, _: usize) {}
 }
 
 // Conjunction modules remember the type of the most recent pulse received from each of their
 // connected input modules; they initially default to remembering a low pulse for each input.
 #[derive(Debug, PartialEq, Eq)]
 struct Conjunction {
-    memory: HashMap<String, Pulse>,
+    memory: HashMap<usize, Pulse>,
 }
 
 impl Conjunction {
@@ -80,8 +92,8 @@ impl Conjunction {
 impl CommunicationModule for Conjunction {
     // When a pulse is received, the conjunction module first updates its memory for that input.
     // Then, if it remembers high pulses for all inputs, it sends a low pulse; otherwise, it sends a high pulse.
-    fn process_pulse(&mut self, name: String, pulse: Pulse) -> Option<Pulse> {
-        self.memory.insert(name, pulse);
+    fn process_pulse(&mut self, module_id: usize, pulse: Pulse) -> Option<Pulse> {
+        self.memory.insert(module_id, pulse);
 
         if self.memory.values().all(|p| *p == Pulse::High) {
             Some(Pulse::Low)
@@ -90,27 +102,25 @@ impl CommunicationModule for Conjunction {
         }
     }
 
-    fn register_input_module(&mut self, name: String) {
-        self.memory.insert(name, Pulse::Low);
+    fn register_input_module(&mut self, module_id: usize) {
+        self.memory.insert(module_id, Pulse::Low);
     }
 }
 
-impl Hash for Conjunction {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        let mut hasher = DefaultHasher::new();
-        for (key, _) in &self.memory {
-            key.hash(&mut hasher);
-        }
-        state.write_u64(hasher.finish());
-    }
-}
-
-fn part_1(lines: Lines) -> usize {
-    let mut modules: HashMap<String, Rc<dyn CommunicationModule>> = HashMap::new();
-    // let mut flip_flops: HashMap<String, Rc<FlipFlop>, Vec<String>> = HashMap::new();
-    // let mut conjunctions: HashMap<String, Rc<Conjunction>, Vec<String>> = HashMap::new();
+fn parse_input(
+    lines: Lines,
+) -> (
+    Vec<Box<dyn CommunicationModule>>,
+    Vec<Vec<usize>>,
+    Broadcaster,
+) {
+    // All modules are stored in a vec. Their index in the vec is their ID.
+    let mut modules: Vec<Box<dyn CommunicationModule>> = Vec::new();
+    // Vec of module names to be able to map module names to module IDs/indexes.
+    let mut module_names: Vec<String> = Vec::new();
+    // Map of module names to list of names of their destinations. After all
+    // input is parsed, this is converted to a nested vec of module IDs for easy indexing.
     let mut module_destinations: HashMap<String, Vec<String>> = HashMap::new();
-
     // There is a single broadcast module. When it receives a pulse,
     // it sends the same pulse to all of its destination modules.
     let mut broadcaster: Vec<String> = Vec::new();
@@ -121,15 +131,16 @@ fn part_1(lines: Lines) -> usize {
                 .split(", ")
                 .map(|s| s.to_string())
                 .collect::<Vec<String>>();
+
             match module.split_at(1) {
                 ("%", name) => {
-                    let ff = Rc::new(FlipFlop::new());
-                    modules.insert(name.to_string(), ff.clone());
+                    modules.push(Box::new(FlipFlop::new()));
+                    module_names.push(name.to_string());
                     module_destinations.insert(name.to_string(), destinations);
                 }
                 ("&", name) => {
-                    let con = Rc::new(Conjunction::new());
-                    modules.insert(name.to_string(), con.clone());
+                    modules.push(Box::new(Conjunction::new()));
+                    module_names.push(name.to_string());
                     module_destinations.insert(name.to_string(), destinations);
                 }
                 ("b", _) => {
@@ -141,21 +152,53 @@ fn part_1(lines: Lines) -> usize {
         _ => panic!("Invalid line {:?}", line),
     });
 
-    // module_destinations.iter().for_each(|(name, destinations)| {
-    //     destinations.iter().for_each(|d| {
-    //         modules
-    //             .get(d)
-    //             .unwrap()
-    //             .borrow_mut()
-    //             .register_input_module(name.clone())
-    //     })
-    // });
-
-    // dbg!(modules.keys().collect::<Vec<&String>>());
-    modules
+    // After parsing, convert broadcaster to vec of module IDs
+    let broadcaster: Broadcaster = broadcaster
         .iter()
-        .for_each(|(name, module)| println!("Module {:?}", name));
-    dbg!(broadcaster);
+        .map(|name| module_names.iter().position(|n| n == name).unwrap())
+        .collect();
+
+    // After parsing, convert module destinations to a nested vec of module IDs.
+    // Each entry contains the destinations for the module at the index of the entry.
+    let module_destinations: Vec<Vec<usize>> = module_names
+        .iter()
+        .map(|name| {
+            module_destinations
+                .get(name)
+                .unwrap()
+                .iter()
+                .map(|destination| {
+                    // If destination does not exist, create a sink module so that pulses can still be sent and counted.
+                    if let Some(index) = module_names.iter().position(|n| n == destination) {
+                        index
+                    } else {
+                        modules.push(Box::new(Sink {}));
+                        modules.len() - 1
+                    }
+                })
+                .collect()
+        })
+        .collect();
+
+    // Register input modules to initialize the memory of the Conjunction modules.
+    module_destinations
+        .iter()
+        .enumerate()
+        .for_each(|(source_index, destinations)| {
+            destinations.iter().for_each(|&destination_index| {
+                modules[destination_index].register_input_module(source_index);
+            });
+        });
+
+    (modules, module_destinations, broadcaster)
+}
+
+fn part_1(lines: Lines) -> usize {
+    let (modules, module_destinations, broadcaster) = parse_input(lines);
+
+    dbg!(&modules);
+    dbg!(&module_destinations);
+    dbg!(&broadcaster);
     0
 }
 

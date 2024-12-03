33f6943dd43d84e1504949873842cf0324ed6a21
Armin <halilovic.armin@hotmail.com>
2024-12-03 10:04:33 +0100

    day 3 part 2

--- src/y2024/day03.rs
+++ src/y2024/day03.rs
@@ -1,38 +1,44 @@
 use crate::util::util::load_input;
 use crate::{Solution, SolutionPair};
-use std::str::Lines;
+use regex::Regex;
 use std::usize;
 
-use regex::Regex;
-
-// How many reports are safe?
-fn part_1(lines: Lines) -> usize {
-    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
-
-    lines
-        .map(|line| {
-            re.captures_iter(line)
-                .map(|c| c.extract())
-                .map(|(_, [left, right])| {
-                    left.parse::<usize>().expect("could not parse left number")
-                        * right
-                            .parse::<usize>()
-                            .expect("could not parse right number")
-                })
-                .sum::<usize>()
-        })
-        .sum()
+fn parse_num(left: &str) -> usize {
+    left.parse::<usize>().expect("could not parse number")
 }
 
-fn part_2(lines: Lines) -> i32 {
-    0
+// Add up all the results of the mult operations
+fn part_1(input: &str) -> usize {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    re.captures_iter(&input)
+        .map(|c| c.extract().1)
+        .map(|[left, right]| parse_num(left) * parse_num(right))
+        .sum::<usize>()
+}
+
+// The most recent do() or don't() instruction determines whether to apply the mul()
+fn part_2(input: &str) -> usize {
+    let re = Regex::new(r"(?m)(do|don't)(\(\))|mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    re.captures_iter(&input)
+        .map(|c| c.extract().1)
+        .fold(("do", 0), |(last, result), [left, right]| match left {
+            "do" => (left, result),
+            "don't" => (left, result),
+            _ => match last {
+                "do" => (last, result + parse_num(left) * parse_num(right)),
+                _ => (last, result),
+            },
+        })
+        .1
 }
 
 pub fn solve() -> SolutionPair {
     let input = load_input("inputs/2024/day_2");
     (
-        Solution::from(part_1(input.lines())),
-        Solution::from(part_2(input.lines())),
+        Solution::from(part_1(&input)),
+        Solution::from(part_2(&input)),
     )
 }
 
@@ -42,24 +48,26 @@ mod tests {
 
     const EXAMPLE_INPUT: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const EXAMPLE_INPUT_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_part_1_example() {
-        assert_eq!(part_1(EXAMPLE_INPUT.lines()), 161);
+        assert_eq!(part_1(&EXAMPLE_INPUT), 161);
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(load_input("inputs/2024/day_3").lines()), 173517243);
+        assert_eq!(part_1(&load_input("inputs/2024/day_3")), 173517243);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT.lines()), 4);
-    // }
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT_2), 48);
+    }
 
-    // #[test]
-    // fn test_part_2() {
-    //     assert_eq!(part_2(load_input("inputs/2024/day_3").lines()), 271);
-    // }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&load_input("inputs/2024/day_3")), 100450138);
+    }
 }

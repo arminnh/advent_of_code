78585510f263e9ad6d3b4f9ffa4b8f0b27fee769
Armin <haliovic.armin@hotmail.com>
2024-12-04 12:42:14 +0100

    day 4 part 1 cleanup

--- src/y2024/day04.rs
+++ src/y2024/day04.rs
@@ -3,7 +3,10 @@ use crate::{Solution, SolutionPair};
 use std::str::Lines;
 use std::usize;
 
-fn directions() -> Vec<(i32, i32)> {
+type Position = (i32, i32);
+type Direction = (i32, i32);
+
+fn directions() -> Vec<Direction> {
     vec![
         (1, 0),
         (1, 1),
@@ -16,82 +19,28 @@ fn directions() -> Vec<(i32, i32)> {
     ]
 }
 
-fn neighbors(x: i32, y: i32, max_x: i32, max_y: i32) -> Vec<(i32, i32)> {
-    vec![
-        (x + 1, y),
-        (x + 1, y + 1),
-        (x + 1, y - 1),
-        (x - 1, y),
-        (x - 1, y + 1),
-        (x - 1, y - 1),
-        (x, y + 1),
-        (x, y - 1),
-    ]
-    .into_iter()
-    .filter(|&(x, y)| x > 0 && x < max_x && y > 0 && y < max_y)
-    .collect()
+fn in_bounds(pos: Position, max_x: i32, max_y: i32) -> bool {
+    pos.0 >= 0 && pos.0 < max_x && pos.1 >= 0 && pos.1 < max_y
 }
 
 fn search_in_direction(
     grid: &Vec<Vec<char>>,
-    x: i32,
-    y: i32,
-    dir_x: i32,
-    dir_y: i32,
+    mut pos: Position,
+    dir: Direction,
     max_x: i32,
     max_y: i32,
-    rest_of_word: &str,
+    word: &str,
 ) -> usize {
-    if rest_of_word.is_empty() {
-        return 1;
+    for c in word.chars() {
+        pos = (pos.0 + dir.0, pos.1 + dir.1);
+        if !in_bounds(pos, max_x, max_y) || grid[pos.0 as usize][pos.1 as usize] != c {
+            return 0;
+        }
     }
-    let next_char = rest_of_word.chars().next().unwrap();
-    let next_x = x + dir_x;
-    let next_y = y + dir_y;
-    if next_x < 0 || next_x >= max_x || next_y < 0 || next_y >= max_y {
-        return 0;
-    }
-    if grid[next_x as usize][next_y as usize] == next_char {
-        return search_in_direction(
-            grid,
-            next_x,
-            next_y,
-            dir_x,
-            dir_y,
-            max_x,
-            max_y,
-            &rest_of_word[1..],
-        );
-    }
-    0
+    1
 }
 
-fn count_occurences(
-    grid: &Vec<Vec<char>>,
-    x: i32,
-    y: i32,
-    max_x: i32,
-    max_y: i32,
-    rest_of_word: &str,
-) -> usize {
-    println!(
-        "{:?}, {}: {:?}",
-        (x, y),
-        grid[x as usize][y as usize],
-        rest_of_word
-    );
-    if rest_of_word.is_empty() {
-        return 1;
-    }
-    directions()
-        .into_iter()
-        .map(|(dir_x, dir_y)| {
-            search_in_direction(grid, x, y, dir_x, dir_y, max_x, max_y, rest_of_word)
-        })
-        .sum()
-}
-
-// Add up all the results of the mult operations
+// How many times does XMAS appear?
 fn part_1(lines: Lines) -> usize {
     let grid: Vec<Vec<char>> = lines.map(|l| l.chars().collect()).collect();
     let max_x = grid.len();
@@ -101,18 +50,43 @@ fn part_1(lines: Lines) -> usize {
     for x in 0..max_x {
         for y in 0..max_y {
             if grid[x][y] == 'X' {
-                let cnt =
-                    count_occurences(&grid, x as i32, y as i32, max_x as i32, max_y as i32, "MAS");
-                println!("FOUND FROM {:?}: {}", (x, y), cnt);
-                result += cnt;
+                result += directions()
+                    .into_iter()
+                    .map(|dir| {
+                        search_in_direction(
+                            &grid,
+                            (x as i32, y as i32),
+                            dir,
+                            max_x as i32,
+                            max_y as i32,
+                            "MAS",
+                        )
+                    })
+                    .sum::<usize>();
             }
         }
     }
     result
 }
 
-// The most recent do() or don't() instruction determines whether to apply the mul()
+// How many times does MAS appear in the shape of an X?
 fn part_2(lines: Lines) -> usize {
+    // let grid: Vec<Vec<char>> = lines.map(|l| l.chars().collect()).collect();
+    // let max_x = grid.len();
+    // let max_y = grid[0].len();
+
+    // let mut result = 0;
+    // for x in 0..max_x {
+    //     for y in 0..max_y {
+    //         if grid[x][y] == 'X' {
+    //             let cnt =
+    //                 count_occurences(&grid, x as i32, y as i32, max_x as i32, max_y as i32, "MAS");
+    //             println!("FOUND FROM {:?}: {}", (x, y), cnt);
+    //             result += cnt;
+    //         }
+    //     }
+    // }
+    // result
     0
 }
 
@@ -156,6 +130,10 @@ S.S.S.S.SS
 ..M.M.M.MM
 .X.X.XMASX";
 
+    const EXAMPLE_INPUT_4: &str = "M.S
+.A.
+M.S";
+
     #[test]
     fn test_part_1_example() {
         assert_eq!(part_1(EXAMPLE_INPUT_1.lines()), 4);
@@ -168,10 +146,10 @@ S.S.S.S.SS
         assert_eq!(part_1(load_input("inputs/2024/day_4").lines()), 2524);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT_2.lines()), 48);
-    // }
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT_4.lines()), 1);
+    }
 
     // #[test]
     // fn test_part_2() {

915597a765125b428f337d122061733e87398250
Armin <haliovic.armin@hotmail.com>
2024-12-08 21:22:29 +0100

    prepared files for 2024

---
+++ src/y2024/day20.rs
@@ -0,0 +1,47 @@
+use crate::util::util::load_input;
+use crate::{Solution, SolutionPair};
+use std::str::Lines;
+use std::usize;
+
+fn part_1(lines: Lines) -> usize {
+    0
+}
+
+fn part_2(lines: Lines) -> usize {
+    0
+}
+
+pub fn solve() -> SolutionPair {
+    let input = load_input("inputs/2024/day_20");
+    (
+        Solution::from(part_1(input.lines())),
+        Solution::from(part_2(input.lines())),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "";
+
+    #[test]
+    fn test_part_1_example() {
+        assert_eq!(part_1(EXAMPLE_INPUT.lines()), 0);
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(load_input("inputs/2024/day_20").lines()), 0);
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT.lines()), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(load_input("inputs/2024/day_20").lines()), 0)
+    }
+}

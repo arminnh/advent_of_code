6816a6c154cb5dfdb007561f2ee390dc45003d17
Armin <haliovic.armin@hotmail.com>
2024-12-08 21:11:18 +0100

    day 8 cleanup

--- src/y2024/day08.rs
+++ src/y2024/day08.rs
@@ -4,8 +4,10 @@ use std::collections::{HashMap, HashSet};
 use std::str::Lines;
 use std::usize;
 
-fn parse_input(lines: Lines) -> (HashMap<char, Vec<(i32, i32)>>, i32, i32) {
-    let mut antennas: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
+type Position = (i32, i32);
+
+fn parse_input(lines: Lines) -> (HashMap<char, Vec<Position>>, i32, i32) {
+    let mut antennas: HashMap<char, Vec<Position>> = HashMap::new();
     let mut max_x = 0;
     let mut max_y = 0;
     for (x, line) in lines.enumerate() {
@@ -21,38 +23,60 @@ fn parse_input(lines: Lines) -> (HashMap<char, Vec<(i32, i32)>>, i32, i32) {
     (antennas, max_x, max_y)
 }
 
-fn find_antinodes(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<(i32, i32)> {
-    // points are visited row by row, left to right
-    let dx = (x1 - x2) as i32;
-    let dy = (y1 - y2) as i32;
+fn find_closest_antinodes(pos1: Position, pos2: Position) -> [Position; 2] {
+    let dx = pos1.0 - pos2.0;
+    let dy = pos1.1 - pos2.1;
 
-    vec![(x1 + dx, y1 + dy), (x2 - dx, y2 - dy)]
+    [(pos1.0 + dx, pos1.1 + dy), (pos2.0 - dx, pos2.1 - dy)]
 }
 
-fn in_bounds(x: i32, y: i32, max_x: i32, max_y: i32) -> bool {
-    x >= 0 && x < max_x && y >= 0 && y < max_y
+fn in_bounds(pos: Position, max_x: i32, max_y: i32) -> bool {
+    pos.0 >= 0 && pos.0 < max_x && pos.1 >= 0 && pos.1 < max_y
+}
+
+fn find_all_antinodes(positions: Vec<Position>, max_x: i32, max_y: i32) -> Vec<Position> {
+    let mut antinodes = Vec::new();
+
+    for (i, (x1, y1)) in positions.iter().enumerate() {
+        for (x2, y2) in &positions[i + 1..] {
+            let (dx, dy) = (x1 - x2, y1 - y2);
+
+            let mut next_pos = (x1 + dx, y1 + dy);
+            while in_bounds(next_pos, max_x, max_y) {
+                antinodes.push(next_pos);
+                next_pos = (next_pos.0 + dx, next_pos.1 + dy);
+            }
+
+            next_pos = (x2 - dx, y2 - dy);
+            while in_bounds(next_pos, max_x, max_y) {
+                antinodes.push(next_pos);
+                next_pos = (next_pos.0 - dx, next_pos.1 - dy);
+            }
+        }
+    }
+
+    antinodes
 }
 
 fn part_1(lines: Lines) -> usize {
     let (antennas, max_x, max_y) = parse_input(lines);
+    let mut antinodes: HashSet<Position> = HashSet::new();
 
-    antennas
-        .into_iter()
-        .map(|(_, positions)| {
-            let mut out = Vec::new();
-            for (i, (x1, y1)) in positions.iter().enumerate() {
-                for (x2, y2) in &positions[i + 1..] {
-                    find_antinodes(*x1, *y1, *x2, *y2)
-                        .into_iter()
-                        .filter(|pos| in_bounds(pos.0, pos.1, max_x, max_y))
-                        .for_each(|pos| out.push(pos));
+    for (_, positions) in antennas {
+        for (i, &pos1) in positions.iter().enumerate() {
+            for &pos2 in &positions[i + 1..] {
+                let [first, second] = find_closest_antinodes(pos1, pos2);
+                if in_bounds(first, max_x, max_y) {
+                    antinodes.insert(first);
+                }
+                if in_bounds(second, max_x, max_y) {
+                    antinodes.insert(second);
                 }
             }
-            out
-        })
-        .flatten()
-        .collect::<HashSet<_>>()
-        .len()
+        }
+    }
+
+    antinodes.len()
 }
 
 fn part_2(lines: Lines) -> usize {
@@ -61,27 +85,13 @@ fn part_2(lines: Lines) -> usize {
     antennas
         .into_iter()
         .filter(|(_, positions)| positions.len() > 1)
-        .map(|(f, positions)| {
-            let mut out = positions.clone();
-            for (i, (x1, y1)) in positions.iter().enumerate() {
-                for (x2, y2) in &positions[i + 1..] {
-                    let (dx, dy) = ((x1 - x2) as i32, (y1 - y2) as i32);
-                    let mut next_pos = (x1 + dx, y1 + dy);
-                    while in_bounds(next_pos.0, next_pos.1, max_x, max_y) {
-                        out.push(next_pos);
-                        next_pos = (next_pos.0 + dx, next_pos.1 + dy);
-                    }
-
-                    let mut next_pos = (x2 - dx, y2 - dy);
-                    while in_bounds(next_pos.0, next_pos.1, max_x, max_y) {
-                        out.push(next_pos);
-                        next_pos = (next_pos.0 - dx, next_pos.1 - dy);
-                    }
-                }
-            }
-            out
+        .flat_map(|(_, positions)| {
+            positions
+                .clone()
+                .into_iter()
+                .chain(find_all_antinodes(positions, max_x, max_y))
+                .collect::<Vec<_>>()
         })
-        .flatten()
         .collect::<HashSet<_>>()
         .len()
 }
@@ -164,9 +174,9 @@ mod tests {
 
     #[test]
     fn test_find_antinode() {
-        assert_eq!(find_antinodes(3, 4, 5, 5), vec![(1, 3), (7, 6)]);
-        assert_eq!(find_antinodes(3, 4, 4, 8), vec![(2, 0), (5, 12)]);
-        assert_eq!(find_antinodes(5, 5, 4, 8), vec![(6, 2), (3, 11)]);
+        assert_eq!(find_closest_antinodes((3, 4), (5, 5)), [(1, 3), (7, 6)]);
+        assert_eq!(find_closest_antinodes((3, 4), (4, 8)), [(2, 0), (5, 12)]);
+        assert_eq!(find_closest_antinodes((5, 5), (4, 8)), [(6, 2), (3, 11)]);
     }
 
     #[test]
@@ -182,6 +192,6 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(load_input("inputs/2024/day_8").lines()), 0)
+        assert_eq!(part_2(load_input("inputs/2024/day_8").lines()), 951)
     }
 }

09b4d85e8c43a167aa1540397f9de276ebb2043b
Armin <haliovic.armin@hotmail.com>
2024-12-13 11:28:47 +0100

    day 13 part 2

--- src/y2024/day13.rs
+++ src/y2024/day13.rs
@@ -1,25 +1,25 @@
 use crate::util::util::load_input;
 use crate::{Solution, SolutionPair};
-use std::usize;
+use std::u64;
 
-type Coord = (usize, usize);
+type Coord = (u64, u64);
 
 struct Line {
     // y = m x + b
-    m: f32,
-    b: f32,
+    m: f64,
+    b: f64,
 }
 
 impl Line {
-    fn from_point_slope_form(p: Coord, slope: (usize, usize)) -> Self {
-        let slope: f32 = slope.1 as f32 / slope.0 as f32;
+    fn from_point_slope_form(p: Coord, slope: (u64, u64)) -> Self {
+        let slope: f64 = slope.1 as f64 / slope.0 as f64;
         Line {
             m: slope,
-            b: p.1 as f32 - slope * p.0 as f32,
+            b: p.1 as f64 - slope * p.0 as f64,
         }
     }
 
-    fn intersection(&self, other: &Line) -> Option<(f32, f32)> {
+    fn intersection(&self, other: &Line) -> Option<(f64, f64)> {
         if self.m == other.m {
             return None;
         }
@@ -70,41 +70,35 @@ fn parse_machine(input: &str) -> (Coord, Coord, Coord) {
 }
 
 // Smallest number of presses of buttons A and B to get to the Prize
-fn min_tokens_to_win_price(a: Coord, b: Coord, prize: Coord) -> usize {
-    // look for intersection between origin with slope A and prize with slope B
+fn min_tokens_to_win_price(a: Coord, b: Coord, prize: Coord) -> u64 {
+    // Look for intersection between origin with slope A and prize with slope B
+    // From origin to intersection == steps of A, intersection to prize == steps of B
     let origin_and_a = Line::from_point_slope_form((0, 0), a);
     let prize_and_b = Line::from_point_slope_form(prize, b);
 
     if let Some(intersection) = origin_and_a.intersection(&prize_and_b) {
-        let (inters_x, inters_y) = (
-            intersection.0.round() as usize,
-            intersection.1.round() as usize,
-        );
-        if inters_x > prize.0 || inters_y > prize.1 {
-            // overshot the target
-            return 0;
-        }
+        let (inters_x, inters_y) = (intersection.0.round() as u64, intersection.1.round() as u64);
 
-        if inters_x % a.0 != 0 || (inters_x / a.0 != inters_y / a.1) {
-            // nr of A presses cannot be determined
-            return 0;
-        }
-        let a_steps = inters_x / a.0;
-
-        if (prize.0 - inters_x) % b.0 != 0
-            || ((prize.0 - inters_x) / b.0 != (prize.1 - inters_y) / b.1)
+        if (inters_x > prize.0 || inters_y > prize.1) // overshot the target
+            // nr of A presses is not whole number
+            || inters_x % a.0 != 0
+            // nr of B presses is not whole number
+            || (prize.0 - inters_x) % b.0 != 0
+            // nr of steps is different in one dimension (because of rounding?)
+            || (prize.0 - inters_x) / b.0 != (prize.1 - inters_y) / b.1
         {
-            // nr of A presses cannot be determined
             return 0;
         }
-        let b_steps = (prize.0 - inters_x) / b.0;
 
+        let a_steps = inters_x / a.0;
+        let b_steps = (prize.0 - inters_x) / b.0;
         return a_steps * 3 + b_steps;
     }
     0
 }
 
-fn part_1(input: String) -> usize {
+// What is the fewest tokens you would have to spend to win all possible prizes?
+fn part_1(input: String) -> u64 {
     input
         .split("\n\n")
         .map(|s| parse_machine(s))
@@ -112,8 +106,19 @@ fn part_1(input: String) -> usize {
         .sum()
 }
 
-fn part_2(input: String) -> usize {
-    0
+// The prizes are much farther away
+fn part_2(input: String) -> u64 {
+    input
+        .split("\n\n")
+        .map(|s| parse_machine(s))
+        .map(|(a, b, prize)| {
+            min_tokens_to_win_price(
+                a,
+                b,
+                (prize.0 + 10_000_000_000_000, prize.1 + 10_000_000_000_000),
+            )
+        })
+        .sum()
 }
 
 pub fn solve() -> SolutionPair {
@@ -186,12 +191,7 @@ Prize: X=18641, Y=10279",
     }
 
     #[test]
-    fn test_part_2_example() {
-        assert_eq!(part_2(EXAMPLE_INPUT.to_string()), 0);
-    }
-
-    #[test]
     fn test_part_2() {
-        assert_eq!(part_2(load_input("inputs/2024/day_13")), 0)
+        assert_eq!(part_2(load_input("inputs/2024/day_13")), 85527711500010)
     }
 }

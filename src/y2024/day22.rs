0f4210e48aaa86668a131ac442855c3af5f5fdcb
Armin <halilovic.armin@hotmail.com>
2024-12-24 18:31:47 +0100

    day 22 part 1

--- src/y2024/day22.rs
+++ src/y2024/day22.rs
@@ -3,8 +3,33 @@ use crate::{Solution, SolutionPair};
 use std::str::Lines;
 use std::usize;
 
-fn part_1(lines: Lines) -> usize {
-    0
+fn mix(a: u64, b: u64) -> u64 {
+    a ^ b
+}
+
+fn prune(val: u64) -> u64 {
+    val % 16777216
+}
+
+fn n_iterations(mut secret: u64, n: usize) -> u64 {
+    for _ in 0..n {
+        let mult = secret as u64 * 64;
+        secret = mix(secret, mult);
+        secret = prune(secret);
+        let div = secret / 32;
+        secret = mix(secret, div);
+        secret = prune(secret);
+        let mult2 = secret * 2048;
+        secret = mix(secret, mult2);
+        secret = prune(secret);
+    }
+    secret
+}
+
+fn part_1(lines: Lines) -> u64 {
+    lines
+        .map(|line| n_iterations(line.parse().expect("Could not parse secret number"), 2000))
+        .sum()
 }
 
 fn part_2(lines: Lines) -> usize {
@@ -23,11 +48,32 @@ pub fn solve() -> SolutionPair {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT: &str = "";
+    const EXAMPLE_INPUT: &str = "1
+10
+100
+2024";
+
+    #[test]
+    fn test_n_iterations() {
+        assert_eq!(n_iterations(123, 1), 15887950);
+        assert_eq!(n_iterations(123, 2), 16495136);
+        assert_eq!(n_iterations(123, 3), 527345);
+        assert_eq!(n_iterations(123, 4), 704524);
+        assert_eq!(n_iterations(123, 5), 1553684);
+        assert_eq!(n_iterations(123, 6), 12683156);
+        assert_eq!(n_iterations(123, 7), 11100544);
+        assert_eq!(n_iterations(123, 8), 12249484);
+        assert_eq!(n_iterations(123, 9), 7753432);
+        assert_eq!(n_iterations(123, 10), 5908254);
+        assert_eq!(n_iterations(1, 2000), 8685429);
+        assert_eq!(n_iterations(10, 2000), 4700978);
+        assert_eq!(n_iterations(100, 2000), 15273692);
+        assert_eq!(n_iterations(2024, 2000), 8667524);
+    }
 
     #[test]
     fn test_part_1_example() {
-        assert_eq!(part_1(EXAMPLE_INPUT.lines()), 0);
+        assert_eq!(part_1(EXAMPLE_INPUT.lines()), 37327623);
     }
 
     #[test]

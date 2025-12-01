65c777072979015b231ccfe0ae45bf5ab2edd0b1
Armin <halilovic.armin@hotmail.com>
2025-12-01 20:21:07 +0100

    placeholder coming days

---
+++ src/y2025/day04.rs
@@ -0,0 +1,38 @@
+// ...
+pub fn part_1(input: &str) -> usize {
+    0
+}
+
+// ...
+pub fn part_2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::util::util::load_input;
+
+    use super::*;
+
+    const EXAMPLE_INPUT_1: &str = "";
+
+    #[test]
+    fn test_part_1_example() {
+        assert_eq!(part_1(EXAMPLE_INPUT_1), 0);
+    }
+
+    // #[test]
+    // fn test_part_1() {
+    //     assert_eq!(part_1(&load_input("inputs/2025/day_4")), 0);
+    // }
+
+    // #[test]
+    // fn test_part_2_example() {
+    //     assert_eq!(part_2(EXAMPLE_INPUT_1), 0);
+    // }
+
+    // #[test]
+    // fn test_part_2() {
+    //     assert_eq!(part_2(&load_input("inputs/2025/day_4")), 0);
+    // }
+}

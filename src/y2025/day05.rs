549cf5fbc0305a9d3ac716a2761cac1e7b51ddaf
Armin <halilovic.armin@hotmail.com>
2025-12-05 11:27:51 +0100

    day 5 part 2 -  0.2 ms

--- src/y2025/day05.rs
+++ src/y2025/day05.rs
@@ -36,9 +36,47 @@ fn is_fresh(id: &usize, ranges: &Vec<(usize, usize)>) -> bool {
     false
 }
 
-// ...
+// How many possible ingredient IDs could be considered fresh?
 pub fn part_2(input: &str) -> usize {
-    0
+    let (ranges, _) = input
+        .split_once("\n\n")
+        .expect("Could not split input in 2 parts");
+    let ranges = parse_ranges(ranges);
+    let ranges = merge_overlapping_ranges(ranges);
+    ranges.iter().map(|(start, end)| end - start + 1).sum()
+}
+
+fn merge_overlapping_ranges(ranges: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+    let mut new_ranges = Vec::new();
+
+    for (start, end) in ranges {
+        if let Some(p) = new_ranges
+            .iter()
+            .position(|(s, e)| overlaps((start, end), (*s, *e)))
+        {
+            let (prev_start, prev_end) = new_ranges[p];
+            new_ranges[p] = merge_ranges(prev_start, prev_end, start, end);
+            new_ranges = merge_overlapping_ranges(new_ranges);
+        } else {
+            new_ranges.push((start, end));
+        }
+    }
+
+    new_ranges
+}
+
+fn merge_ranges(prev_start: usize, prev_end: usize, start: usize, end: usize) -> (usize, usize) {
+    let new_start = if prev_start < start {
+        prev_start
+    } else {
+        start
+    };
+    let new_end = if prev_end < end { end } else { prev_end };
+    (new_start, new_end)
+}
+
+fn overlaps(a: (usize, usize), b: (usize, usize)) -> bool {
+    a.0 <= b.1 && b.0 <= a.1
 }
 
 #[cfg(test)]
@@ -59,6 +97,22 @@ mod tests {
 17
 32";
 
+    const EXAMPLE_INPUT_2: &str = "3-5
+10-14
+16-20
+12-18
+3-5
+3-6
+2-6
+3-5
+8-9
+9-10
+7-8
+6-8
+0-25
+
+";
+
     #[test]
     fn test_part_1_example() {
         assert_eq!(part_1(EXAMPLE_INPUT_1), 3);
@@ -66,16 +120,17 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(&load_input("inputs/2025/day_5")), 0);
+        assert_eq!(part_1(&load_input("inputs/2025/day_5")), 888);
     }
 
-    // #[test]
-    // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT_1), 0);
-    // }
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(part_2(EXAMPLE_INPUT_1), 14);
+        assert_eq!(part_2(EXAMPLE_INPUT_2), 26);
+    }
 
-    // #[test]
-    // fn test_part_2() {
-    //     assert_eq!(part_2(&load_input("inputs/2025/day_5")), 0);
-    // }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&load_input("inputs/2025/day_5")), 344378119285354);
+    }
 }

776ac4bb76d2d2a0d8333487888da368ed1c3243
Armin <halilovic.armin@hotmail.com>
2025-12-10 14:27:47 +0100

    day 10 parse joltage for part 2

--- src/y2025/day10.rs
+++ src/y2025/day10.rs
@@ -1,17 +1,15 @@
 #[derive(Debug)]
 struct Machine {
-    lights: u8,            // nr of lights, max 10
-    target: Vec<bool>,     // target configuration of lights
-    buttons: Vec<Vec<u8>>, // each button is a list of lights it toggles
-    joltage: Vec<u8>,
+    lights: u8,               // nr of lights, max 10
+    lights_target: Vec<bool>, // target configuration of lights
+    buttons: Vec<Vec<u8>>,    // each button is a list of lights it toggles
+    joltage_target: Vec<u16>, // part 2: buttons increment joltage counters instead of toggling lights
 }
 
 impl From<&str> for Machine {
     fn from(value: &str) -> Self {
-        let mut parts = value.split_whitespace();
-        let target: Vec<bool> = parts
-            .next()
-            .expect("Could not find lights config")
+        let parts: Vec<&str> = value.split_whitespace().collect();
+        let lights: Vec<bool> = parts[0]
             .chars()
             .filter_map(|c| {
                 if c == '#' {
@@ -23,8 +21,8 @@ impl From<&str> for Machine {
                 }
             })
             .collect();
-        let buttons: Vec<Vec<u8>> = parts
-            .take_while(|s| s.starts_with('('))
+        let buttons: Vec<Vec<u8>> = parts[1..parts.len() - 1]
+            .iter()
             .map(|s| {
                 s[1..s.len() - 1] // skip brackets
                     .split(',')
@@ -32,12 +30,20 @@ impl From<&str> for Machine {
                     .collect()
             })
             .collect();
+        let joltage_str = parts[parts.len() - 1];
+        let joltage: Vec<u16> = joltage_str[1..joltage_str.len() - 1]
+            .split(',')
+            .map(|c| c.parse::<u16>().expect("Could not parse joltage nr"))
+            .collect();
+        if lights.len() != joltage.len() {
+            panic!("Nr of lights does not equal nr of joltage counters");
+        }
 
         Machine {
-            lights: target.len() as u8,
-            target: target,
+            lights: lights.len() as u8,
+            lights_target: lights,
             buttons: buttons,
-            joltage: Vec::new(),
+            joltage_target: joltage,
         }
     }
 }
@@ -59,7 +65,7 @@ fn fewest_button_presses(machine: Machine) -> usize {
     let mut states: Vec<Vec<bool>> = Vec::from([start_state]);
     // let mut paths: VecDeque<(Vec<bool>)> =
     //     machine.buttons.iter().map(|b| (start_state, *b)).collect();
-    while !states.contains(&machine.target) {
+    while !states.contains(&machine.lights_target) {
         let mut next_states = Vec::new();
         for s in states {
             for b in &machine.buttons {
@@ -79,7 +85,7 @@ fn next_state(mut state: Vec<bool>, button: &Vec<u8>) -> Vec<bool> {
     state
 }
 
-//
+// What is the fewest button presses required to correctly configure the joltage level counters on all of the machines?
 pub fn part_2(input: &str) -> usize {
     0
 }
@@ -106,7 +112,7 @@ mod tests {
 
     // #[test]
     // fn test_part_2_example() {
-    //     assert_eq!(part_2(EXAMPLE_INPUT_1), 0);
+    //     assert_eq!(part_2(EXAMPLE_INPUT_1), 33);
     // }
 
     // #[test]
